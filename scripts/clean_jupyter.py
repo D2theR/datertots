c14bfb8cdb619c2dd6d840b7d523da3468b75cad
ian <ian@tpastream.com>
2020-05-02 18:47:12 -0400

    now with headers...

--- scripts/clean_jupyter.py
+++ scripts/clean_jupyter.py
@@ -1,13 +1,27 @@
 from bs4 import BeautifulSoup
 with open('post.html') as f:
-    soup=BeautifulSoup(f, 'lxml')
+    post=BeautifulSoup(f, 'lxml')
+with open('static_site/index.html') as f:
+    index=BeautifulSoup(f, 'lxml')
 
-for ele in soup.find_all("div", class_="prompt input_prompt"):
+nav_bar = index.find('nav')
+
+last_div=[div for div in post.body.find_all('div')][-1]
+first_div=post.body.find('div')
+first_div.insert_before(nav_bar)
+for link in index.find_all('link'):
+    last_div.insert_after(link)
+for script in index.find_all('script'):
+    last_div.insert_after(script)
+
+
+for ele in post.find_all("div", class_="prompt input_prompt"):
     ele.decompose()
 
-for ele in soup.find_all("div", class_="prompt output_prompt"):
+for ele in post.find_all("div", class_="prompt output_prompt"):
     ele.decompose()
+
 
 with open('out.html','w') as o:
-    o.write(str(soup.html))
+    o.write(str(post.html))
 
